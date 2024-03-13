1e0bf72f8730a239311bda538700d4b2a30754e0
Ayush Chittranshi <ayushchittranshi.0@gmail.com>
2024-03-13 10:02:01 +0530

    Added prettier and other initial setup

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = express()
 
 const PORT = process.env.PORT || 3000;
 
+app.use("/assets",express.static(__dirname+'/static'))
 app.set('view engine','ejs')
 app.get('/',(req,res)=>{
     res.render("index")
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+    content: ["./**/*.{html,js,jsx,ejs}"],
   theme: {
     extend: {},
   },
